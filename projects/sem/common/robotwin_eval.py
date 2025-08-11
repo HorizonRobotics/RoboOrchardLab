177a5e3ccebd8b50ece0bc7aea95084a4187794d
xuewu.lin <xuewu.lin@horizon.cc>
2025-08-11 17:58:18 +0800

    add robotwin2.0 eval

---
+++ projects/sem/common/robotwin_eval.py
@@ -0,0 +1,124 @@
+import argparse
+import json
+import logging
+import multiprocessing
+import os
+import re
+import subprocess
+
+import torch
+from robo_orchard_lab.utils import log_basic_config
+
+bash_command_template = (
+    "export CUDA_VISIBLE_DEVICES={gpu_id} \n"
+    "python3 script/eval_policy.py --config sem_policy/deploy_policy.yml "
+    "  --overrides "
+    "  --task_config {task_config} "
+    "  --task_name {task_name} "
+    "  --vlm_ckpt_dir {vlm_ckpt_dir} "
+    "  --urdf_dir {urdf_dir} "
+    "  --model_config {model_config} "
+    "  --test_num {test_num}"
+)
+
+
+def eval_tasks(gpu_id, task_names, args, results=None):
+    if_cluster = os.environ.get("CLUSTER") is not None
+    if results is None:
+        results = {}
+    for task_name in task_names:
+        if not if_cluster:
+            log_dir = f"eval_result/{task_name}/{args.task_config}"
+        else:
+            log_dir = f"/job_data/{task_name}/{args.task_config}"
+        os.makedirs(log_dir, exist_ok=True)
+        log_file = os.path.join(log_dir, "log.txt")
+        logger.info(f"Start to eval task[{task_name}], log file: {log_file}")
+        command = bash_command_template.format(
+            gpu_id=gpu_id,
+            task_name=task_name,
+            task_config=args.task_config,
+            vlm_ckpt_dir=args.vlm_ckpt_dir,
+            urdf_dir=args.urdf_dir,
+            model_config=args.model_config,
+            test_num=args.test_num,
+        )
+        with open(log_file, "w", encoding="utf-8") as f:
+            ret = subprocess.run(
+                command, shell=True, stdout=f, stderr=subprocess.STDOUT
+            )
+        if ret.returncode == 0:
+            with open(log_file, "r", encoding="utf-8") as f:
+                output = f.read().strip().split("\n")
+            for out in output[::-1]:
+                if "Success rate" in out:
+                    results[task_name] = float(
+                        re.findall(r"\d+\.?\d+%", out)[0][:-1]
+                    )
+                    logger.info(f"{task_name}: {out}")
+                    break
+        else:
+            logger.info(
+                f"Fail to eval task[{task_name}] with returncode {ret.returncode}"
+            )
+    return results
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger(__file__)
+    log_basic_config(
+        format="%rank %(asctime)s %(levelname)s %(filename)s:%(lineno)d | %(message)s",  # noqa: E501
+        level=logging.INFO,
+    )
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--task_config", type=str)
+    parser.add_argument("--model_config", type=str)
+    parser.add_argument("--task_names", type=str, default=None)
+    parser.add_argument("--vlm_ckpt_dir", type=str, default=None)
+    parser.add_argument("--urdf_dir", type=str, default=None)
+    parser.add_argument("--test_num", type=int, default=100)
+    args = parser.parse_args()
+
+    task_names = args.task_names
+    all_task_names = [
+        x[:-3]
+        for x in os.listdir("envs")
+        if x.endswith(".py") and not x.startswith("_")
+    ]
+    if task_names is None:
+        task_names = all_task_names
+    else:
+        task_names = task_names.strip().split(",")
+
+    num_gpus = torch.cuda.device_count()
+    task_names_allocated = [[] for _ in range(min(num_gpus, len(task_names)))]
+    for index, task_name in enumerate(task_names):
+        if task_name not in all_task_names:
+            logger.warning(f"Task {task_name} is invalid !!")
+            continue
+        task_names_allocated[index % num_gpus].append(task_name)
+
+    if len(task_names_allocated) > 1:
+        processes = []
+        manager = multiprocessing.Manager()
+        results = manager.dict()
+        for gpu_id, task_names_per_gpu in enumerate(task_names_allocated):
+            logger.info(f"{gpu_id}: {task_names_per_gpu}")
+            p = multiprocessing.Process(
+                target=eval_tasks,
+                args=(gpu_id, task_names_per_gpu, args, results),
+            )
+            p.start()
+            processes.append(p)
+
+        for p in processes:
+            p.join()
+        results = dict(results)
+    else:
+        results = eval_tasks(0, task_names_allocated[0], args)
+    mean_success_rate = sum(list(results.values())) / len(results)
+    results["num_tasks"] = len(results)
+    results["mean"] = mean_success_rate
+    results["test_num_per_task"] = args.test_num
+    logger.info(json.dumps(results, indent=4))
---
+++ projects/sem/common/sem_robotwin_policy/deploy_policy.py
@@ -0,0 +1,173 @@
+# Project RoboOrchard
+#
+# Copyright (c) 2024-2025 Horizon Robotics. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+import copy
+import importlib
+import logging
+import os
+
+import numpy as np
+import requests
+import torch
+
+from robo_orchard_core.utils.config import load_config_class
+from robo_orchard_lab.models.mixin import ModelMixin
+from robo_orchard_lab.utils.path import in_cwd
+
+current_file_path = os.path.abspath(__file__)
+parent_directory = os.path.dirname(current_file_path)
+
+logger = logging.getLogger(__file__)
+
+
+def load_config(config_file):
+    assert config_file.endswith(".py")
+    module_name = os.path.split(config_file)[-1][:-3]
+    spec = importlib.util.spec_from_file_location(module_name, config_file)
+    config = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(config)
+    return config
+
+
+def download_job_ckpt_processor(ckpt_url, processor_name, output_dir="./model"):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir, exist_ok=True)
+
+    while ckpt_url.endswith("/"):
+        ckpt_url = ckpt_url[:-1]
+    model_url = f"{ckpt_url}/model.safetensors"
+    model_config_url = f"{ckpt_url}/model.config.json"
+    processor_url = '/'.join(ckpt_url.split('/')[:-2] + [f"{processor_name}.json"])
+    print(
+        f"model_ckpt: {model_url}\n"
+        f"model_config: {model_config_url}\n"
+        f"procssor: {processor_url}"
+    )
+    for url in [model_url, model_config_url, processor_url]:
+        file_name = os.path.join(output_dir, url.split("/")[-1])
+        if os.path.exists(file_name):
+            continue
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            with open(file_name, "wb") as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+
+
+class SEMPolicy:
+    def __init__(self, config, processor=None, vlm_ckpt_dir=None, urdf_dir=None):
+        if processor is None:
+            processor = "processor"
+        if config.startswith("http"):
+            download_job_ckpt_processor(
+                ckpt_url=config,
+                processor_name=processor,
+                output_dir="./sem_eval_model"
+            )
+            config = "./sem_eval_model"
+        logger.info(f"model config: {config}, processor: {processor}")
+
+        target_vlm_ckpt_dir = os.path.join(config, "ckpt")
+        target_urdf_dir = os.path.join(config, "urdf")
+        if vlm_ckpt_dir is not None and not os.path.exists(target_vlm_ckpt_dir):
+            os.symlink(vlm_ckpt_dir, target_vlm_ckpt_dir)
+        if urdf_dir is not None and not os.path.exists(target_urdf_dir):
+            os.symlink(urdf_dir, target_urdf_dir)
+
+        processor_cfg = load_config_class(
+            open(os.path.join(config, f"{processor}.json")).read()
+        )
+        with in_cwd(config):
+            self.processor = processor_cfg()
+
+        self.model = ModelMixin.load_model(config)
+        self.model.eval()
+        self.model.requires_grad_()
+
+        self.device = torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu"
+        )
+        self.model.to(self.device)
+        self.take_action_cnt = 0
+
+    def data_preprocess(self, obs, instruction):
+        images = []
+        depths = []
+        T_world2cam = []  # noqa: N806
+        intrinsic = []
+        for camera_data in obs["observation"].values():
+            images.append(camera_data["rgb"])
+            depths.append(camera_data["depth"] / 1000)
+
+            _tmp = np.eye(4)
+            _tmp[:3] = camera_data["extrinsic_cv"]
+            T_world2cam.append(_tmp)
+
+            _tmp = np.eye(4)
+            _tmp[:3, :3] = camera_data["intrinsic_cv"]
+            intrinsic.append(_tmp)
+
+        joint_state = []
+        joint_action = obs["joint_action"]
+        joint_action = (
+            joint_action["left_arm"]
+            + [joint_action["left_gripper"]]
+            + joint_action["right_arm"]
+            + [joint_action["right_gripper"]]
+        )
+        joint_state.append(joint_action)
+        data = dict(
+            intrinsic=np.stack(intrinsic),
+            T_world2cam=np.stack(T_world2cam),
+            step_index=len(joint_state) - 1,
+            joint_state=np.stack(joint_state),
+        )
+        data["imgs"] = np.stack(images)
+        data["depths"] = np.stack(depths)
+        data["text"] = instruction
+        data = self.processor.pre_process(data)
+        return data
+
+    def get_action(self, observation, instruction):
+        data = self.data_preprocess(observation, instruction)
+        model_outs = self.model(data)
+        actions = self.processor.post_process(data, model_outs).action
+        valid_action_step = 32
+        actions = actions[:valid_action_step].cpu().numpy()
+        return actions
+
+
+def get_model(usr_args):  # from your deploy_policy.yml
+    policy = SEMPolicy(
+        usr_args["model_config"],
+        usr_args["model_processor"],
+        usr_args["vlm_ckpt_dir"],
+        usr_args["urdf_dir"],
+    )
+    return policy
+
+
+def eval(task_env, model, observation):
+    instruction = task_env.get_instruction()
+    actions = model.get_action(observation, instruction)
+
+    for action in actions:  # Execute each step of the action
+        observation = task_env.get_obs()
+        task_env.take_action(action)
+
+
+def reset_model(model):
+    pass
