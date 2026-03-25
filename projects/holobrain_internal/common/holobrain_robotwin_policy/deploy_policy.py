fix(holobrain/robotwin_policy): disable gradients in inference mode

projects/holobrain_internal/common/holobrain_robotwin_policy/deploy_policy.py
@@ -145,7 +145,7 @@ class HoloBrainPolicy:
         self.processor = HoloBrainProcessor.load(config, f"{processor}.json")
         self.model = ModelMixin.load_model(config, load_impl="native")
         self.model.eval()
-        self.model.requires_grad_()
+        self.model.requires_grad_(False)
 
         self.device = torch.device(
             "cuda" if torch.cuda.is_available() else "cpu"